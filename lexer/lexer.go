c6c616f0b9330aae71af12ae205b32c06b62d58b
Raoul Christian Wograndl <Wograndl.Raoul@outlook.com>
2024-07-08 22:29:58 +0200

    feat: implement lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,143 @@
+package lexer
+
+import (
+	"monkey/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int // Current position in input (points to the current char)
+	readPosition int // Current reading position in input (points to after the current char)
+	char         byte
+}
+
+func New(input string) *Lexer {
+	lexer := &Lexer{input: input}
+	lexer.readChar()
+	return lexer
+}
+
+func (lexer *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	lexer.skipWhitespace()
+
+	switch lexer.char {
+	case '=':
+		if lexer.peekChar() == '=' {
+			char := lexer.char
+			lexer.readChar()
+			literal := string(char) + string(lexer.char)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, lexer.char)
+		}
+	case ';':
+		tok = newToken(token.SEMICOLON, lexer.char)
+	case '(':
+		tok = newToken(token.LPAREN, lexer.char)
+	case ')':
+		tok = newToken(token.RPAREN, lexer.char)
+	case ',':
+		tok = newToken(token.COMMA, lexer.char)
+	case '+':
+		tok = newToken(token.PLUS, lexer.char)
+	case '-':
+		tok = newToken(token.MINUS, lexer.char)
+	case '!':
+		if lexer.peekChar() == '=' {
+			char := lexer.char
+			lexer.readChar()
+			literal := string(char) + string(lexer.char)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, lexer.char)
+		}
+	case '/':
+		tok = newToken(token.SLASH, lexer.char)
+	case '*':
+		tok = newToken(token.ASTERISK, lexer.char)
+	case '<':
+		tok = newToken(token.LT, lexer.char)
+	case '>':
+		tok = newToken(token.GT, lexer.char)
+	case '{':
+		tok = newToken(token.LBRACE, lexer.char)
+	case '}':
+		tok = newToken(token.RBRACE, lexer.char)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(lexer.char) {
+			tok.Literal = lexer.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(lexer.char) {
+			tok.Type = token.INT
+			tok.Literal = lexer.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lexer.char)
+		}
+	}
+	lexer.readChar()
+
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.char = 0 // NULL character
+	} else {
+		l.char = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (lexer *Lexer) readIdentifier() string {
+	position := lexer.position
+	for isLetter(lexer.char) {
+		lexer.readChar()
+	}
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) skipWhitespace() {
+	for isWhitespace(lexer.char) {
+		lexer.readChar()
+	}
+}
+
+func (lexer *Lexer) readNumber() string {
+	position := lexer.position
+	for isDigit(lexer.char) {
+		lexer.readChar()
+	}
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) peekChar() byte {
+	if lexer.readPosition >= len(lexer.input) {
+		return 0
+	} else {
+		return lexer.input[lexer.readPosition]
+	}
+}
+
+func newToken(tokenType token.TokenType, char byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(char)}
+}
+
+func isLetter(char byte) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+func isDigit(char byte) bool {
+	return '0' <= char && char <= '9'
+}
+
+func isWhitespace(char byte) bool {
+	return char == ' ' || char == '\t' || char == '\n' || char == '\r'
+}
